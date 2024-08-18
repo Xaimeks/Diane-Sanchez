1ef4132d8f7faa50ef0c891f701c9eb2a279c324
Ex <xaimeksgg123@gmail.com>
2024-08-18 15:51:41 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+- **`# Project Name`**: Diane Sanchez
+- **`## Description`**: A voice in your ears that will intermittently voice your actions to you and tell you how bad you are.
+- **`## Installation`**: A little later
+- **`## Usage`**: A little later
+- **`## Functions`**: Until I get access to a good chat bot API, it will just be a boilerplate 'voice assistant' if you can call it that.
+In the future I plan to take this to a larger scale and make a handy assistant without an app, using Rick's wife as a base.
+- **`## Configurations`**: A little later
+- **`## Requirements`**: A little later
+- **`## Contacts`**: tg: Xaimeks
---
+++ main.py
@@ -0,0 +1,34 @@
+import psutil as ps 
+import time
+
+def all_proc():
+  for proc in ps.process_iter(['pid', 'name', 'username']):
+    try:
+      print(f'PID: {proc.info['pid']}, Name: {proc.info['name']}, User: {proc.info['username']}')
+    except (ps.NoSuchProcess, ps.AccessDenied, ps.ZombieProcess):
+      print('Oops..')
+      
+# new process monitoring
+def monitor_new_process():
+  prev_proc = set(p.info['pid'] for p in ps.process_iter(['pid']))
+  processed_names = set()
+    
+  while True:
+    
+    time.sleep(1)
+    curr_proc = set(p.info['pid'] for p in ps.process_iter(['pid']))
+    new_proc = curr_proc - prev_proc
+      
+    for element in new_proc:
+      process = ps.Process(element)
+      proc_name = process.name()
+      
+      if proc_name not in processed_names:
+        print(f'New Process: {proc_name}')
+        processed_names.add(proc_name)
+        
+    prev_proc = curr_proc
+
+
+all_proc()
+monitor_new_process()
